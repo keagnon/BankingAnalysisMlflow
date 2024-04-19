0dc410ca9f6c3c34a2f1713093702d47599bbe4f
keagnon <keagnon.gbe@gmail.com>
2024-04-19 15:24:57 +0200

    Updating notebook file for data analysis

--- scripts/deployment.py
+++ scripts/deployment.py
@@ -1,24 +1,18 @@
 import mlflow
 from mlflow_utils import create_mlflow_experiment
 
-# Définition de la fonction, assurez-vous qu'elle est dans votre script ou importée correctement
-def create_mlflow_experiment(
-    experiment_name="ProjetMLFLOW",
-    artifact_location="testing_mlflow_artifact",
-    tags={"env": "dev", "version": "1.0.0"}
-) -> str:
-    try:
-        experiment_id = mlflow.create_experiment(
-            name=experiment_name, artifact_location=artifact_location, tags=tags
-        )
-    except Exception as e:
-        print(f"Experiment {experiment_name} already exists: {str(e)}")
-        experiment_id = mlflow.get_experiment_by_name(experiment_name).experiment_id
+if __name__=="__main__":
 
-    mlflow.set_experiment(experiment_name=experiment_name)
-
-    return experiment_id
-
-# Appel de la fonction sans passer d'arguments car les valeurs par défaut sont utilisées
-# experiment_id = create_mlflow_experiment()
+    experiment_id = create_mlflow_experiment(
+        experiment_name="ProjetMLFLOW",
+        artifact_location="testing_mlflow_artifact",
+        tags={"env": "dev", "version": "1.0.0"},
+    )
 # print(f"Experiment ID: {experiment_id}")
+
+client = MlflowClient()
+with mlflow.start_run(run_name="first_run_rf") as run:
+    mlflow.sklearn.log_model(artifact_path="rfr_model", sk_model=RandomForestRegressor(), registered_model_name="RandomForestRegressor")
+    model_name = "RandomForest"
+    # create registered model
+    client.create_registered_model(model_name)
